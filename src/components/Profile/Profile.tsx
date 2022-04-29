c3f03f6c96ece2b67e432790684d8540e8319bd9
SvetaK7 <atevs7@inbox.ru>
2022-04-29 14:04:58 +0300

    lesson 15

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
+
+const Profile = () => {
+    return (
+        <div className={s.content}>
+            <div>
+                <img src="https://i.ytimg.com/vi/ditSdFr-KGA/maxresdefault.jpg" alt="photo"></img>
+            </div>
+            <div>ava + description</div>
+            <MyPosts />
+        </div>
+    );
+}
+
+export default Profile;
