77e22d4d8d817f16d244d84cbc325c3734b4a507
SvetaK7 <atevs7@inbox.ru>
2022-04-30 10:42:41 +0300

    lesson 19 routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Header from "./components/Header/Header";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 
 function App() {
     return (
-        <div className='app-wrapper'>
-            <Header />
-            <Navbar />
-            <div className = 'app-wrapper-content'>
-                <Dialogs />
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path={"/dialogs"} component={Dialogs}/>
+                    <Route path={"/profile"} component={Profile}/>
+                </div>
             </div>
+        </BrowserRouter>
 
-            {/*<Profile />*/}
-        </div>
     );
 }
 
