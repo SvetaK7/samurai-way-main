39f02cc9216402b90ddb5dcb05649b72bfad150c
SvetaK7 <atevs7@inbox.ru>
2022-05-05 11:51:11 +0300

    lesson 24 Array object

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,11 +4,11 @@ import {NavLink} from "react-router-dom";
 
 type DialogItemType = {
     name: string;
-    id: string;
+    id: number;
 }
 const DialogItem = (props: DialogItemType) => {
     return (
-        <div className={s.dialog + ' ' + s.active}>
+        <div className={s.dialog + " " + s.active}>
             <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
         </div>
     )
@@ -23,20 +23,42 @@ const Message = (props: MessagePropsType) => {
 }
 
 const Dialogs = () => {
+
+    let dialogsData = [
+        {id: 1, name: 'Dima'},
+        {id: 2, name: 'Sveta'},
+        {id: 3, name: 'Artem'},
+        {id: 4, name: 'Egor'},
+        {id: 5, name: 'Victor'},
+        {id: 6, name: 'Valera'}
+    ]
+
+    let messagesData = [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Hey'},
+        {id: 4, message: 'I am happy'},
+        {id: 5, message: 'You are welcome'},
+        {id: 6, message: 'I am cooking pizza today'}
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={"Dima"} id={"1"}/>
-                <DialogItem name={"Sveta"} id={"2"}/>
-                <DialogItem name={"Artem"} id={"3"}/>
-                <DialogItem name={"Egor"} id={"4"}/>
-                <DialogItem name={"Victor"} id={"5"}/>
-                <DialogItem name={"Valera"} id={"6"}/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={"Sveta"} id={dialogsData[1].id}/>
+                <DialogItem name={"Artem"} id={dialogsData[2].id}/>
+                <DialogItem name={"Egor"} id={dialogsData[3].id}/>
+                <DialogItem name={"Victor"} id={dialogsData[4].id}/>
+                <DialogItem name={"Valera"} id={dialogsData[5].id}/>
             </div>
             <div className={s.messages}>
-                <Message message={"Hello"}/>
-                <Message message={"How are you?"}/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
                 <Message message={"Hey"}/>
+                <Message message={"I am happy"}/>
+                <Message message={"You are welcome"}/>
+                <Message message={"I am cooking pizza today"}/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,12 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    let postsData = [
+        {id: 1, message: 'Hi, how are you', likesCount: 12},
+        {id: 2, message: 'It is my first post', likesCount: 11},
+   ]
+
     return (
             <div className={s.postsBlock}>
                 <h3>My posts</h3>
@@ -14,8 +20,8 @@ const MyPosts = () => {
                         <button>Add post</button>
                     </div>
                     <div className = {s.posts}>
-                        <Post message = {'Hi, how are you'} likeCount = {8}/>
-                        <Post message = {'It is my first post'} likeCount = {15}/>
+                        <Post message = {postsData[0].message} likeCount = {postsData[0].likesCount}/>
+                        <Post message = {postsData[1].message} likeCount = {postsData[1].likesCount}/>
                     </div>
                 </div>
 
