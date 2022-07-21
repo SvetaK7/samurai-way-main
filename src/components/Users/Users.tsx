2c7cf48f14c6f68d39172b4f4040312414dcaea0
SvetaK7 <atevs7@inbox.ru>
2022-07-21 15:43:00 +0300

    lesson 54

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -14,9 +14,7 @@ type UsersType = {
 
 export class Users extends React.Component<any, any> {
 
-    constructor(props: any) {
-        super(props);
-
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             this.props.setUsers(response.data.items)
         });
