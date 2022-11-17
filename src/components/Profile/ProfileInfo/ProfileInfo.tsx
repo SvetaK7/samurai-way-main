21d081bb0420d05acfa2b4dca8fa3370e60c4d65
SvetaK7 <atevs7@inbox.ru>
2022-11-17 21:40:07 +0300

    71, setstate

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import {Preloader} from "../../common/Preloader/Preloader";
+import {ProfileStatus} from "./ProfileStatus";
 
 const ProfileInfo = (props: any) => {
-    if(!props.profile){
+    if (!props.profile) {
         return <Preloader/>
     }
 
     return (
-        <div>
-            <div>
-                <img src="https://i.ytimg.com/vi/ditSdFr-KGA/maxresdefault.jpg" alt="photo"></img>
-            </div>
-            <div className={s.descriptionBlock}>
-                <img src={props.profile.photos.small}/>
-                ava + description
-            </div>
-        </div>
+        <ProfileStatus status={'hey'}/>
     );
 }
 
