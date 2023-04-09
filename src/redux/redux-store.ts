ef462d87ad1e9c894ea4c9d1d4e9f9af73bfde66
SvetaK7 <atevs7@inbox.ru>
2023-04-09 14:36:38 +0300

    add redux dev tools, lesson 91

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import {profileReducer} from "./profile-reducer";
 import {dialogsReducer} from "./dialogs-reducer";
 import {sidebarReducer} from "./sidebar-reducer";
@@ -20,4 +20,10 @@ const reducers = combineReducers({
 })
 
 export type AppStateType2 = ReturnType<typeof reducers>
-export const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+
+//@ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+//@ts-ignore
+window.store = store;
